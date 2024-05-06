14a40c665840599375d56d2f3920b440cd4a0810
abane <abanesebiane@outlook.com>
2024-05-06 21:40:31 +0200

    initial commit

---
+++ src/components/layout/header.tsx
@@ -0,0 +1,26 @@
+import CurrentUser from "./current-user"
+import { Layout, Space } from "antd"
+
+
+const Header = () => {
+  const headerStyle: React.CSSProperties = {
+    background: "#fff",
+    display: "flex",
+    justifyContent: "flex-end",
+    alignItems: "center",
+    padding: "0 24px",
+    position: 'sticky',
+    top: 0,
+    zIndex: 999,
+  }
+
+  return (
+    <Layout.Header style={headerStyle}>
+      <Space align="center" size="middle">
+        <CurrentUser />
+      </Space>
+    </Layout.Header>
+  )
+}
+
+export default Header
